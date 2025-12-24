c15bc1777551bbbb0d1c050e9bfdcab10e1f4d8a
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-24 11:41:53 -0600

    Add bcryptjs and mongodb dependencies to package.json and package-lock.json

---
+++ app/api/auth/singup.js
@@ -0,0 +1,35 @@
+import { connectToDatabase } from '@/lib/db';
+import { verifyPassword } from '@/lib/auth';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
+
+    if (!email || !email.includes('@') || !password || password.trim() === '') 
+    {
+        return res.status(422).json({ message: 'Invalid input - email or password is missing.' });
+    }
+
+    if (password.trim().length < 7) {
+        return res.status(422).json({ message: 'Invalid input - password should be at least 7 characters long.' });
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db('authentication');
+
+    const existingUser = await db.collection('users').findOne({ email: email });
+    if (existingUser) {
+        client.close();
+        return res.status(422).json({ message: 'User already exists.' });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection('users').insertOne({ email: email, password: hashedPassword });
+
+    client.close();
+
+    res.status(201).json({ message: 'Signed up successfully', userId: result.insertedId });
+  }
+  res.status(200).json({ message: 'Signup successful' });
+}
