781a051c2d6ed02dabccefcf471aec8f3f3da552
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-27 09:55:08 -0600

    Implement user session management in UserProfile: Added loading state and redirection logic to handle authentication status using NextAuth, ensuring users are redirected to the auth page if not authenticated.

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,8 +1,33 @@
 import classes from './user-profile.module.css';
 import ProfileForm from './profile-form';
+import { useSession, getSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        router.push('/auth');
+      }else{
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (status === 'unauthenticated') {
+    return <p>Access Denied</p>;
+  }
 
   return (
     <section className={classes.profile}>
