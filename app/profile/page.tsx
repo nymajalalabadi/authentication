d46797e5a8260c69f0ac1ffb4f56bf228228efaa
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-28 15:06:42 -0600

    Add session handling in ProfilePage: Implemented loading and access denied states using NextAuth to manage user authentication status, enhancing user experience.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
 import UserProfile from '@/components/profile/user-profile';
+import { useSession } from 'next-auth/react';
 
 export default function ProfilePage() {
+  const { status } = useSession();
+
+  if (status === 'unauthenticated') {
+    return <p>Access Denied</p>;
+  }
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
   return <UserProfile />;
 }
